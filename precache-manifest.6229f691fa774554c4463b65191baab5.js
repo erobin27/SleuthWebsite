f399fab018578367e1e1d7cda1c2979ad8e4e554
erobin27 <elijah1998robinson@gmail.com>
2020-10-23 19:43:00 -0400

    Updates

---
+++ precache-manifest.6229f691fa774554c4463b65191baab5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a56b91bf7038e8f9bfe72621e0da4be",
+    "url": "/SleuthWebsite/index.html"
+  },
+  {
+    "revision": "00e8b03ec20e6055f198",
+    "url": "/SleuthWebsite/static/css/main.85834b48.chunk.css"
+  },
+  {
+    "revision": "50967dd5877ef0f5b902",
+    "url": "/SleuthWebsite/static/js/2.282d5ef2.chunk.js"
+  },
+  {
+    "revision": "768278ee9385f9e2af3713be2359c57b",
+    "url": "/SleuthWebsite/static/js/2.282d5ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00e8b03ec20e6055f198",
+    "url": "/SleuthWebsite/static/js/main.4e4b7b24.chunk.js"
+  },
+  {
+    "revision": "7176bc0d20af6fa2f96f",
+    "url": "/SleuthWebsite/static/js/runtime-main.6b2c492b.js"
+  }
+]);
